4590aa06c49d3c31bc468166c240121cf13e682c
Hamza Rauf <hamza_leopard@live.com>
2020-02-25 00:31:00 +0500

    Initial commit

---
+++ Movie Rental System/Movie Rental System/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using Movie_Rental_System.Models;
+using Movie_Rental_System.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movie_Rental_System.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shawshank"
+            };
+
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Customer1" },
+                new Customer {Name = "Customer2" }
+
+            };
+
+            var viewmodel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+
+
+            };
+
+
+
+                 return View(viewmodel);
+
+        }
+
+    }
+}
---
+++ Movie Rental System/Movie Rental System/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Movie_Rental_System.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+
+    }
+}
