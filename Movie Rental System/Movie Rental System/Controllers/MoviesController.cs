0092640012f984f3c264d5244737f4f3dabeee12
Hamza Rauf <hamza_leopard@live.com>
2020-03-03 17:34:51 +0500

    Movie module form completed with error

--- Movie Rental System/Movie Rental System/Controllers/MoviesController.cs
+++ Movie Rental System/Movie Rental System/Controllers/MoviesController.cs
@@ -43,8 +43,32 @@ namespace Movie_Rental_System.Controllers
             {
                 Genres = genres
             };
-
+            ViewData["Id"] = "0";
             return View("New",viewmodel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                _context.Movies.Add(movie);
+            }
+
+            else
+            {
+                var new_Movie = _context.Movies.Single(m => m.Id == movie.Id);
+                new_Movie.Name = movie.Name;
+                new_Movie.ReleaseDate = movie.ReleaseDate;
+                new_Movie.Genre = movie.Genre;
+                new_Movie.NumberInStock = movie.NumberInStock;
+
+
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
 
         public ActionResult MovieDetails(int Id)
@@ -59,6 +83,23 @@ namespace Movie_Rental_System.Controllers
             }
 
             return View(movie);
+        }
+        public ActionResult Edit(int Id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == Id);
+            var viewmodel = new NewMovieViewModel
+            {
+                Movies = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            ViewData["Id"] = "1";
+
+            return View("New", viewmodel);
         }
 
     }
